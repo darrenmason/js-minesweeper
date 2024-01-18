59b4338aabe93086286f8dcf83f7606762a2148d
Darren Mason <mason.darren@gmail.com>
2024-01-18 17:46:02 -0500

    edge detection

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const flagsLeft = document.querySelector('#flags-left')
   const width = 10
   let bombAmount = 20
+  let squares = []
 
   function createBoard() {
 
@@ -12,15 +13,40 @@ document.addEventListener('DOMContentLoaded', () => {
     const emptyArray = Array(width * width - bombAmount).fill('valid')
     const gameArray = emptyArray.concat(bombArray)
     const shuffledArray = gameArray.sort(() => Math.random() - 0.5)
-    console.log(gameArray)
+    //console.log(gameArray)
 
     for (let i = 0; i < width * width; i++){
       const square = document.createElement('div')
       square.id = i
       square.classList.add(shuffledArray[i])
       grid.append(square)
+      squares.push(square)
+      console.log(squares)
+
+      square.addEventListener('click', () => {
+        click(square)
+      })
+
+      // square.addEventListener('click', () => {
+      //   addFlag(square)
+      // })
+    }
+
+    for (let i = 0; i < squares.length; i++){
+      let total = 0
+
+      const isLeftEdge = (i % width === 0)
+      const isRightEdge = (i % width === width - 1)
+
     }
   }
 
   createBoard()
+
+  function click(square) {
+    console.log(square)
+  }
+  function addFlag(square) {
+    console.log(square)
+  }
 });
