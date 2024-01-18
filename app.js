a5f0c3b4e290b9c44e0cb62fb0fe7237fc26b836
Darren Mason <mason.darren@gmail.com>
2024-01-18 15:48:34 -0500

    add border, box sizing

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const grid = document.querySelector('.grid');
-  const flagsLeft = document.querySelector('#flags-left');
-  const width = 10;
-  let bombAmount = 20;
+  const grid = document.querySelector('.grid')
+  const flagsLeft = document.querySelector('#flags-left')
+  const width = 10
+  let bombAmount = 20
 
   function createBoard() {
 
-    flagsLeft.innerHTML = bombAmount;
+    flagsLeft.innerHTML = bombAmount
 
-    const bombArray = Array(bombAmount).fill('bomb');
+    const bombArray = Array(bombAmount).fill('bomb')
     const emptyArray = Array(width * width - bombAmount).fill('valid')
     const gameArray = emptyArray.concat(bombArray)
     const shuffledArray = gameArray.sort(() => Math.random() - 0.5)
-    console.log(gameArray);
+    console.log(gameArray)
 
     for (let i = 0; i < width * width; i++){
-      const square = document.createElement('div');
-      square.id = i;
-      grid.append(square);
+      const square = document.createElement('div')
+      square.id = i
+      square.classList.add(shuffledArray[i])
+      grid.append(square)
     }
   }
 
-  createBoard();
+  createBoard()
 });
